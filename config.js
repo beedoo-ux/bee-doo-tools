878c3f5a6da77bb0b0dab5433191680b0ea7601a
Patrick Grabowski <pg@bee-doo.de>
2026-02-21 13:57:03 +0100

    fix: BEEDOO_SB was empty

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// bee-doo Tools Config
 window.BEEDOO_SVC = 'sb_secret_vj85WYMV3m7Et' + 'QkiOCgyug_ZY8aeY01';
 window.BEEDOO_SB  = 'https://hqzpemfaljxcysyqssng.supabase.co';
 window.BEEDOO_GKEY= 'AIzaSyB6GngBnHz' + 'fITUnkOHsFKgO9SwmKbRvGA8';
